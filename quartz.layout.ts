8e8a141d1a7d12136d4bbee79b080bdb1ff1977d
Dott-rus <78660260+Dott-rus@users.noreply.github.com>
2025-09-15 22:10:21 +0300

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -11,16 +11,18 @@ export const sharedPageComponents: SharedLayout = {
       GitHub: "https://github.com/jackyzha0/quartz",
       "Discord Community": "https://discord.gg/cRFFHYye7t",
     },
-    html: `
+    children: (
+    <>
       <a href="https://notes.somedott.space">Zametki</a> © 2025 <a href="https://somedott.space">Dott</a>. 
       Содержимое доступно по лицензии <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>, если не указано иное (или указан иной источник и его лицензия, проще говоря: Если есть источник, то ищите лицензию там).
-      <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" alt="CC" style="max-width:1em;max-height:1em;margin-left:.2em;">
-      <img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" alt="BY" style="max-width:1em;max-height:1em;margin-left:.2em;">
-      <br>
-      <span style="color: gray">
+      <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" alt="CC" style={{maxWidth: "1em", maxHeight: "1em", marginLeft: ".2em"}} />
+      <img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" alt="BY" style={{maxWidth: "1em", maxHeight: "1em", marginLeft: ".2em"}} />
+      <br />
+      <span style={{color: "gray"}}>
         Пожалуйста, указывайте авторство, если это возможно. Если оно опущено без необходимости — я могу связаться для уточнения.
       </span>
-    `,
+    </>
+  ),
   }),
 }
 
